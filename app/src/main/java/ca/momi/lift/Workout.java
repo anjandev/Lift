1a4c36015982de8c72c37d6076e03d80ff0fbbe9
Anjan Momi <anjan@momi.ca>
2017-04-27 04:10:59 -0700

    Initial commit

---
+++ app/src/main/java/ca/momi/lift/Workout.java
@@ -0,0 +1,13 @@
+package ca.momi.lift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Workout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
+    }
+}
