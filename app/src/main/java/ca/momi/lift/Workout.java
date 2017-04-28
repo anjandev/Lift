ecff0ae5fcd14c9799433b1a76d18b22b55a86d2
Anjan Momi <anjan@momi.ca>
2017-04-27 20:36:05 -0700

    Started new workout code

--- app/src/main/java/ca/momi/lift/Workout.java
+++ app/src/main/java/ca/momi/lift/Workout.java
@@ -2,12 +2,54 @@ package ca.momi.lift;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.support.design.widget.Snackbar;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Workout extends AppCompatActivity {
+
+    public String[] workoutA = {"squat", "bench", "barbellRow"};
+
+    public Map<String, Integer> mapIds(View view){
+        // Create mapping of elements and ids
+        Map<String, Integer> ids = new HashMap<String, Integer>();
+        ids.put("squatSeekbar",R.id.squatSetSeekbar);
+        return ids;
+    }
+
+
+    public void doneSet(View view, int workoutNum) {
+
+        Map<String, Integer> ids = mapIds(view);
+
+        int seekbarId = ids.get(workoutA[workoutNum] + "Seekbar");
+
+        SeekBar numOfSets = (SeekBar)findViewById(seekbarId);
+
+        if(numOfSets.getProgress() < numOfSets.getMax()){
+            numOfSets.setProgress(numOfSets.getProgress() + 1);
+            numOfSets.refreshDrawableState();
+        }
+        else{
+            Snackbar.make(view, "You're done your five sets for " + workoutA[workoutNum], Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        }
+    }
+
+    public void doneSquat(View view) {
+        doneSet(view, 0);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workout);
+        setContentView(R.layout.activity_workouta);
+
+
+
+
     }
 }
---
+++ app/src/main/java/ca/momi/lift/set.java
@@ -0,0 +1,8 @@
+package ca.momi.lift;
+
+/**
+ * Created by anjan on 4/27/2017.
+ */
+
+public class set {
+}
