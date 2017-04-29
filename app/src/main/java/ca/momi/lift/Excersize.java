1170056156647e9f998013722063dda0f9f0d03b
Anjan Momi <anjan@momi.ca>
2017-04-28 18:54:39 -0700

    fixed initializing excersize. Reps are now stored

--- app/src/main/java/ca/momi/lift/Excersize.java
+++ app/src/main/java/ca/momi/lift/Excersize.java
@@ -8,11 +8,14 @@ import java.util.ArrayList;
 
 public class Excersize {
 
-    public static final int NUM_OF_SETS = 5;
-    ArrayList<Integer> sets = new ArrayList<Integer>();
-    String excersizeName;
-    int weight;
-    int setsDone;
+    public static int NUM_OF_SETS = 5;
+    static final String[] workoutA = {"squat", "bench", "barbellRow"};
+
+    public int[] sets = new int[NUM_OF_SETS];
+
+    public String excersizeName;
+    public int weight;
+    public int setsDone;
 
     public void doneSet(int setNum, int repsDone){
         this.set_reps(setNum, repsDone);
@@ -21,31 +24,31 @@ public class Excersize {
 
     public int get_reps(int setNum){
         // set 1 is 0
-        return sets.get(setNum);
+        return sets[setNum];
     }
 
     private void set_reps(int setNum, int repsDone){
-        sets.set(setNum, repsDone);
+        this.sets[setNum] = repsDone;
     }
 
     public int get_setsDone(){
-        return setsDone;
+        return this.setsDone;
     }
 
     public void set_weight(int weightsDone){
-        weight = weightsDone;
+        this.weight = weightsDone;
 
     }
 
     public Excersize(String excersize_sName){
 
-        excersizeName = excersize_sName;
+        this.excersizeName = excersize_sName;
 
-        setsDone = 0;
+        this.setsDone = 0;
 
         // zero data in array
         for(int i = 0; i < NUM_OF_SETS; i++){
-            sets.set(i, 0);
+            this.sets[i] = 0;
         }
 
     }
--- app/src/main/java/ca/momi/lift/Workout.java
+++ app/src/main/java/ca/momi/lift/Workout.java
@@ -12,24 +12,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Workout extends AppCompatActivity {
+    // for debugging
     private static final String TAG = Workout.class.getSimpleName();
-
-
-    // The static initializer is a static {} block of code inside java class, and run only one time before the constructor or main method is called.
-    // "Final" will not allow the variable to be changed
-    static public final String[] workoutA = {"squat", "bench", "barbellRow"};
     static public final int NUM_OF_WORKOUTS = 3;
 
     // Provided that the Map should be shared across every instance of your class, then you need to make it static. You then use a static initialiser block to instantiate and populate it:
     // https://stackoverflow.com/questions/33775273/how-to-declare-hashmap-in-global-and-add-values-into-it-only-for-the-first-time
+    // The static initializer is a static {} block of code inside java class, and run only one time before the constructor or main method is called.
+    // "Final" will not allow the variable to be changed
+    static final String[] workoutA = {"squat", "bench", "barbellRow"};
+    public static Map<String, Excersize> excersizes = new HashMap<String, Excersize>();
 
-    private Map<String, Excersize> createMap() {
-        Map<String, Excersize> excersizes = new HashMap<String, Excersize>();
-        excersizes.put("squat", new Excersize("squat"));
-        return excersizes;
+    // Mapping the ids of the ui elements is not independant of number of excersizes
+    // TODO: write a more general algorithm for this
+    private SeekBar work1;
+    private SeekBar work2;
+    private SeekBar work3;
+
+    static {
+        for(int i = 0; i < NUM_OF_WORKOUTS; i++) {
+            excersizes.put(workoutA[i], new Excersize(workoutA[i]));
+        }
     }
 
-
+    // Mapping the ids of the ui elements is not independant of number of excersizes and names of
+    // excersizes.
+    // TODO: write a more general algorithm for this
     private Map<String, Integer> mapIds(View view){
         // Create mapping of elements and ids
         Map<String, Integer> ids = new HashMap<String, Integer>();
@@ -39,45 +47,42 @@ public class Workout extends AppCompatActivity {
         return ids;
     }
 
-    public void doneSet(View view, int workoutNum) {
+    private void doneSet(View view, int excersizeIdx) {
         /* TODO: change scope so that mapIds function doesnt run everytime done set.
            This is hard because the view must be passed into mapIds
          */
-
-
         Map<String, Integer> ids = mapIds(view);
-        // Map<String, Excersize> excersizes = createMap();
 
-        SeekBar numOfSets = (SeekBar)findViewById(ids.get(workoutA[workoutNum] + "setSeekbar"));
-        SeekBar numOfReps = (SeekBar)findViewById(ids.get(workoutA[workoutNum] + "RepsSeekBar"));
-        // EditText weight = (EditText)findViewById(ids.get(workoutA[workoutNum] + "Weight"));
-        
+        SeekBar numOfSets = (SeekBar)findViewById(ids.get(workoutA[excersizeIdx] + "setSeekbar"));
+        SeekBar numOfReps = (SeekBar)findViewById(ids.get(workoutA[excersizeIdx] + "RepsSeekBar"));
+        EditText weight = (EditText)findViewById(ids.get(workoutA[excersizeIdx] + "Weight"));
 
-
-        //Excersize excersize = excersizes.get(workoutA[workoutNum]);
+        Excersize excersize = excersizes.get(workoutA[excersizeIdx]);
 
         if(numOfSets.getProgress() == 0){
             // set weight here too. Check if empty field
-            //excersize.doneSet(numOfSets.getProgress(), numOfReps.getProgress());
+            excersize.doneSet(numOfSets.getProgress(), numOfReps.getProgress());
             numOfSets.setProgress(numOfSets.getProgress() + 1);
             numOfSets.refreshDrawableState();
 
         }
         else if(numOfSets.getProgress() < numOfSets.getMax()){
-            //excersize.doneSet(numOfSets.getProgress(), numOfReps.getProgress());
+            excersize.doneSet(numOfSets.getProgress(), numOfReps.getProgress());
             numOfSets.setProgress(numOfSets.getProgress() + 1);
             numOfSets.refreshDrawableState();
             // TODO: Add timer
         }
         else{
-            Snackbar.make(view, "You're done your five sets for " + workoutA[workoutNum], Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            Snackbar.make(view, "You're done your five sets for " + workoutA[excersizeIdx], Snackbar.LENGTH_LONG).setAction("Action", null).show();
         }
     }
 
+    // Mapping the ids of the ui elements is not independant of number of excersizes and names of
+    // excersizes.
+    // TODO: write a more general algorithm for this
     public void doneSquat(View view) {
         this.doneSet(view, 0);
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,5 +90,6 @@ public class Workout extends AppCompatActivity {
         // TODO: Use listener and don't allow user to go to sets they havent done yet
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouta);
+
     }
 }
