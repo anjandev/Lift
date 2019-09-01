9724caa4772d57e981f6ba4860e713e32776bfc3
Anjandev Momi <anjan@momi.ca>
2019-09-01 03:13:50 -0700

    make androidx settings

--- app/src/main/java/ca/momi/lift/MainActivity.java
+++ app/src/main/java/ca/momi/lift/MainActivity.java
@@ -166,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent settingIntent = new Intent(MainActivity.this, settings.class);
+            Intent settingIntent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(settingIntent);
             return true;
         } else if (id == R.id.action_about) {
